8cfe2a1b33a4b76c3e14d60ef8416c9fe7b6d753
Andrey Krylov <syatihoko@mail.ru>
2020-11-15 17:49:39 +0300

    20201115 Test Terraform oi 3

--- 07-terraform-03-basic/versions.tf
+++ 07-terraform-03-basic/versions.tf
@@ -10,7 +10,6 @@ terraform {
 
 #s3_bucket для хранения backend s3
 resource "aws_s3_bucket" "kaa-terraform-states" {
-  count = local.ubuntu_instance_workspace_states_map[terraform.workspace]
 bucket = "kaa-terraform-states"
 acl = "private"
 
@@ -23,7 +22,6 @@ tags = {
 
 terraform {
   backend "s3" {
-count = local.ubuntu_instance_workspace_states_map[terraform.workspace]
     bucket = "kaa-terraform-states"
     key = "main-infra/terraform.tfstate"
     region = "eu-west-3"
